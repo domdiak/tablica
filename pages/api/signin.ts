2d61eec4379711f27fcc9b11a57807b7b27646bb
domdiak <domdiak@gmail.com>
2022-05-02 20:37:19 +0200

    added signin api route

---
+++ pages/api/signin.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import prisma from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const { email, password } = req.body;
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                time: Date.now(),
+            },
+            "hello",
+            {
+                expiresIn: "24h",
+            }
+        );
+
+        res.setHeader(
+            "Set-Cookies",
+            cookie.serialize("ACCESS_TOKEN", token, {
+                httpOnly: true,
+                maxAge: 24 * 60 * 60,
+                path: "/",
+                sameSite: "lax",
+                secure: process.env.NODE_ENV === "production",
+            })
+        );
+        res.json(user);
+    } else {
+        res.status(401);
+        res.json({ error: "Email or password is wrong" });
+    }
+};
