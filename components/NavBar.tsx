720dea724a449d7b369722e6031079977ea71c49
domdiak <domdiak@gmail.com>
2022-05-25 11:31:03 +0200

    updated NavBar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,28 @@
 import { Box } from "@chakra-ui/layout";
-import Image from "next/image";
-import { Button, ButtonGroup, Stack } from "@chakra-ui/react";
+import { Button, Stack, Flex } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { Switch } from "@chakra-ui/react";
+import { useState } from "react";
 import Logo from "./Logo";
 
 const NavBar = () => {
     return (
-        <Box height="50px" border="solid 1px">
+        <Box height="70px" border="solid 1px">
             {" "}
             <Box paddingY="5px" paddingX="10px">
-                <Stack spacing={4} direction="row" align="left">
+                <Flex h="16" alignItems="center" justify="space-between">
                     <Logo />
-                    <Button colorScheme="teal"> Logout</Button>
-                    <Button colorScheme="teal">Home</Button>
-                </Stack>
+                    <Flex alignItems="center">
+                        <Stack direction="row" spacing={7}>
+                            <Button colorScheme="teal" leftIcon={<AddIcon />}>
+                                Add Category
+                            </Button>
+                            <Button colorScheme="teal">Show Archive</Button>
+                            <Button colorScheme="teal"> Logout</Button>
+                            <Button colorScheme="teal">Home</Button>
+                        </Stack>
+                    </Flex>
+                </Flex>
             </Box>
         </Box>
     );
